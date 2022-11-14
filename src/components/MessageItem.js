e8bd8344b4444fd6270c6e1b36ab437bea4c85b5
shinework <adrien.baptiste@gmail.com>
2022-11-14 17:00:16 +0100

    switch to functionnal component

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,19 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MessageItem = ({ message }) => (
-  <li
-    style={{
-      marginBottom: 10,
-      backgroundColor: "white",
-      padding: 10,
-      borderRadius: 10,
-    }}
-  >
-    <small>{message.username}</small>
-    <div>{message.body}</div>
-  </li>
-);
+const MessageItem = ({ message }) => {
+  return (
+    <li
+      style={{
+        marginBottom: 10,
+        backgroundColor: "white",
+        padding: 10,
+        borderRadius: 10,
+      }}
+    >
+      <small>{message.username}</small>
+      <div>{message.body}</div>
+    </li>
+  );
+};
 
 MessageItem.propTypes = {
   message: PropTypes.shape({
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,28 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import MessageItem from "./MessageItem";
 
-class MessageList extends Component {
-  constructor(props) {
-    super(props);
+function MessageList() {
+  const [messageItems] = useState([
+    { id: 1, username: "Baptiste", body: "Hello" },
+    { id: 2, username: "John", body: "There" },
+    { id: 3, username: "Doe", body: "Hi there!" },
+  ]);
 
-    this.state = {
-      messageItems: [
-        { id: 1, username: "Baptiste", body: "Hello" },
-        { id: 2, username: "John", body: "There" },
-        { id: 3, username: "Doe", body: "Hi there!" },
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <ul style={{ borderColor: "grey", padding: 10, listStyle: "none" }}>
-        {this.state.messageItems.map((message) => (
-          <MessageItem key={message.id} message={message} />
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul style={{ borderColor: "grey", padding: 10, listStyle: "none" }}>
+      {messageItems.map((message) => (
+        <MessageItem key={message.id} message={message} />
+      ))}
+    </ul>
+  );
 }
 
 export default MessageList;
